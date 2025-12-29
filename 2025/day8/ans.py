8a25066323ab53a9d1e696105444eeec31198f7a
Yash <yash.patel@myriad.com>
2025-12-29 17:37:58 -0600

    day 8 part 2

--- 2025/day8/ans.py
+++ 2025/day8/ans.py
@@ -19,18 +19,9 @@ def merge(circuits, num_connections, seen_circuits, p, q):
         del circuits[key]
     circuits[num_connections] = new_set
 
-def main():
-    f = open('input.txt')
-    lines = f.readlines()
-    coords = getCoords(lines)
-    distances = []
-    pqs = combinations(coords, 2)
-    for pq in pqs:
-        p, q = pq[0], pq[1]
-        distance = getEuclidDistance(p, q)
-        heappush(distances, (distance, p, q))
-    circuits = {}
+def getThreeLargestCircuitSizes(distances):
     num_connections = 0
+    circuits = {}
     while num_connections < 1000:
         distance, p, q = heappop(distances)
         seen_circuits = set()
@@ -47,11 +38,48 @@ def main():
             merge(circuits, num_connections, seen_circuits, p, q)
         num_connections += 1
 
-    print(num_connections)
     circuit_sizes = sorted([len(c) for c in circuits.values()], reverse=True)
-    print(circuit_sizes)
 
     print(circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2])
+
+def getLastJunctionBoxes(num_circuits, distances):
+    p,q = None, None
+    num_connections = 0
+    circuits = {}
+    while not (len(circuits) == 1 and len(list(circuits.values())[0]) == num_circuits):
+        distance, p, q = heappop(distances)
+        seen_circuits = set()
+        for key in circuits:
+            c = circuits[key]
+            if (p in c):
+                seen_circuits.add(key)
+            if (q in c):
+                seen_circuits.add(key)
+
+        if len(seen_circuits) == 0:
+            circuits[num_connections] = {p, q}
+        else:
+            merge(circuits, num_connections, seen_circuits, p, q)
+        num_connections += 1
+    print(circuits) 
+    print(p)
+    print(q)
+    print(p[0] * q[0])
+
+def main():
+    f = open('input.txt')
+    lines = f.readlines()
+    coords = getCoords(lines)
+    distances = []
+    pqs = combinations(coords, 2)
+    for pq in pqs:
+        p, q = pq[0], pq[1]
+        distance = getEuclidDistance(p, q)
+        heappush(distances, (distance, p, q))
+    #getThreeLargestCircuitSizes(distances)
+    num_circuits = len(coords)
+    getLastJunctionBoxes(num_circuits, distances)
+
 
 if __name__ == '__main__':
     main()
